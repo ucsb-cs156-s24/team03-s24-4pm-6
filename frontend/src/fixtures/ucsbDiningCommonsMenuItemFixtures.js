98799b91728bbcdf656b908daafe7b3540f50255
Sawyer Rice <sawyerrice@ucsb.edu>
2024-05-03 11:01:50 -0700

    SR adding fixtures for menuitems

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneMenuItem: {
+        "id": 1,
+        "diningCommonsCode": "DC1",
+        "name": "burger",
+        "station": "GRILL"
+    },
+    threeMenuItems: [
+        {
+            "id": 1,
+            "diningCommonsCode": "DC1",
+            "name": "burger",
+            "station": "GRILL"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "DC2",
+            "name": "salad",
+            "station": "salad bar"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "DC3",
+            "name": "pasta",
+            "station": "Italian"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
