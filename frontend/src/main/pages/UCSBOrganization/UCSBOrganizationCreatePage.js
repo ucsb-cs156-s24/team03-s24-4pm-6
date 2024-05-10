4b7a98e44257040ae4d1815fd5cf480d9321c398
nbelle1 <89555340+nbelle1@users.noreply.github.com>
2024-05-10 13:20:45 -0700

    changes to create page

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.js
@@ -49,4 +49,3 @@ export default function UCSBOrganizationCreatePage({storybook=false}) {
     </BasicLayout>
   )
 }
-
--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -3,14 +3,13 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 export default function UCSBOrganizationIndexPage() {
 
   // Stryker disable all : placeholder for future implementation
-
   return (
     <BasicLayout>
-        <div className="pt-2">
-            <h1>UCSBOrganization not implemented yet</h1>
-            <p><a href="/ucsborganization/create">Create</a></p>
-            <p><a href="/ucsborganization/edit/1">Edit</a></p>
-        </div>
+      <div className="pt-2">
+        <h1>Index page not yet implemented</h1>
+        <p><a href="/placeholder/create">Create</a></p>
+        <p><a href="/placeholder/edit/1">Edit</a></p>
+      </div>
     </BasicLayout>
-);
+  )
 }
