6b32ae7c9a5ec5fe1c7dddfca7cbe5384b022024
Bryce <brycewang@ucsb.edu>
2024-05-10 12:41:44 -0700

    added form for menuitemreview

---
+++ frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
@@ -0,0 +1,170 @@
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
+import { menuItemReviewFixtures } from "fixtures/menuItemReviewFixtures";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Query, QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+
+describe("MenuItemReviewForm tests", () => {
+    const queryClient = new QueryClient();
+
+    const expectedHeaders = ["Item Id", "Reviewer Email", "Stars", "Date Reviewed", "Comments"];
+
+    test("renders correctly with no initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+        
+    });
+
+
+    test("renders correctly when passing in initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm initialContents={menuItemReviewFixtures.oneReview} />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expect(await screen.findByTestId("MenuItemReviewForm-id")).toBeInTheDocument();
+        expect(screen.getByText("Id")).toBeInTheDocument();
+    });
+
+
+    test("that the correct validations are performed", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+        const submitButton = screen.getByText(/Create/);
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Item id is required./);
+        expect(screen.getByText(/Reviewer email is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Stars are required./)).toBeInTheDocument();
+        expect(screen.getByText(/Date reviewed is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Comments are required./)).toBeInTheDocument();
+
+    });
+    
+
+    test("Correct Error messages on missing input", async () => {
+
+        render(
+            <Router  >
+                <MenuItemReviewForm />
+            </Router>
+        );
+        await screen.findByTestId("MenuItemReviewForm-submit");
+        const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Item id is required./);
+        expect(screen.getByText(/Reviewer email is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Stars are required./)).toBeInTheDocument();
+        expect(screen.getByText(/Date reviewed is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Comments are required./)).toBeInTheDocument();
+    });
+
+
+    test("No Error messages on good input", async () => {
+
+        const mockSubmitAction = jest.fn();
+
+
+        render(
+            <Router  >
+                <MenuItemReviewForm submitAction={mockSubmitAction} />
+            </Router>
+        );
+        await screen.findByTestId("MenuItemReviewForm-itemId");
+
+        const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId");
+        const reviewerEmailField = screen.getByTestId("MenuItemReviewForm-reviewerEmail");
+        const starsField = screen.getByTestId("MenuItemReviewForm-stars");
+        const dateReviewedField = screen.getByTestId("MenuItemReviewForm-dateReviewed");
+        const commentsField = screen.getByTestId("MenuItemReviewForm-comments");
+        const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
+
+        fireEvent.change(itemIdField, { target: { value: 1 } });
+        fireEvent.change(reviewerEmailField, { target: { value: "cgaucho@ucsb.edu" } });
+        fireEvent.change(starsField, { target: { value: 5 } });
+        fireEvent.change(dateReviewedField, { target: { value: "2022-01-02T12:00:00" } });
+        fireEvent.change(commentsField, { target: { value: "Good" } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+        expect(screen.queryByText(/Item id is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Reviewer email is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Stars are required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Date reviewed is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Comments are required./)).not.toBeInTheDocument();
+
+    });
+
+
+    test("Correct Error messsages on bad input", async () => {
+
+        render(
+            <Router  >
+                <MenuItemReviewForm />
+            </Router>
+        );
+        await screen.findByTestId("MenuItemReviewForm-dateReviewed");
+        const dateReviewedField = screen.getByTestId("MenuItemReviewForm-dateReviewed");
+        const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
+
+        fireEvent.change(dateReviewedField, { target: { value: 'bad-input' } });
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Date reviewed is required./);
+    });
+    
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+        render(
+            <Router  >
+                <MenuItemReviewForm />
+            </Router>
+        );
+        await screen.findByTestId("MenuItemReviewForm-cancel");
+        const cancelButton = screen.getByTestId("MenuItemReviewForm-cancel");
+
+        fireEvent.click(cancelButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+    });
+});
