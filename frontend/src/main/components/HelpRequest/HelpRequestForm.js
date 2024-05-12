6913756b2d8bfe043490f7913b5efbe0e2366db6
Immanuel Bareket <immanuelbareket@Immanuels-MacBook-Pro.local>
2024-05-12 16:28:17 -0700

    fixed forms

--- frontend/src/main/components/HelpRequest/HelpRequestForm.js
+++ frontend/src/main/components/HelpRequest/HelpRequestForm.js
@@ -23,7 +23,7 @@ function HelpRequestForm({ initialContents, submitAction, buttonLabel = "Create"
     const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
     // Stryker disable next-line all
-    const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
+    //const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
 
     return (
 
